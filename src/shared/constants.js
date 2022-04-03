e9ce4b2d796a9abb9d8745be20753c1b1c93f39d
Aditya Shrivastav <adityashrivastava992001@gmail.com>
2022-04-03 20:36:52 +0530

    added redirect

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,6 @@
 const STUDENT = 'STUDENT';
 const TEACHER = 'TEACHER';
 const ADMIN = 'ADMIN';
-const baseUrl = 'https://papaya-praline-7426cd.netlify.app';
+const baseUrl = 'https://papaya-praline-7426cd.netlify.app/.netlify/functions/index';
 
 export { STUDENT, TEACHER, ADMIN, baseUrl };
